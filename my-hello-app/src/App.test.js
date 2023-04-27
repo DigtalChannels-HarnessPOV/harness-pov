174e5ab4d9ab87957b7ea3b2f07135de03152323
ayomide-shinaaluko <povsandbox@gmail.com>
2023-04-27 11:36:44 +0100

    added Hello world test

--- my-hello-app/src/App.test.js
+++ my-hello-app/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render Hello World Header', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
